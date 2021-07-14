1031260ee010c99b3ba42b61eaff66cd03e02726
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-07-14 11:33:51 -0700

    sdm845-common: packages: Add "remove-packages"
    It's handy, and sorry Camera2 but since we're building
    Lineage Aperture now, I need someone to pick on.

--- common.mk
+++ common.mk
@@ -367,6 +367,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
