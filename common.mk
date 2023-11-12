cd70474694f383faec7037f64b3f3c80a77710fa
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-12 21:07:31 +0100

    sdm845-common: Keep building health HIDL interface lib in vendor
    * GNSS HAL needs it
    
    Change-Id: Id2e1b2844d195d41fc67a0224e0dc39573f7702e

--- common.mk
+++ common.mk
@@ -254,6 +254,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
+    android.hardware.health@2.1.vendor \
     android.hardware.health-service.qti
 
 # HotwordEnrollement app permissions
