20e048ddc0430471b442aa325d004e0c33fa7ff3
Terminator-J <terminator_j@hotbutteredmonkey.com>
2023-12-01 14:54:48 -0800

    sdm845-common: dex: Speed profile preopt all the things!

--- common.mk
+++ common.mk
@@ -196,6 +196,14 @@ PRODUCT_PACKAGES += \
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Launcher3QuickStep \
+    Settings \
+    SettingsGoogle \
+    SystemUI \
+    SystemUIGoogle
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
