ece9ab29311fe1e8952b124368bff9cc9ac71966
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-05-13 19:52:39 -0700

    Revert "[TEST] sdm845-common: Patch camera provider to load the new symbols"
    This reverts commit 91144c1f4f523f93ea022b59d84ba2033c720ad1.
    
    But keep it in history for later, just in case...

--- common.mk
+++ common.mk
@@ -177,9 +177,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor
 
-PRODUCT_PACKAGES += \
-    libcamera_provider_shim
-
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
