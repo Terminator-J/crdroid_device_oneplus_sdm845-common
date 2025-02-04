5844936db6faa689477f01109b887dcc8a159046
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-02-03 17:00:27 -0800

    sdm845-common: usb: Fixup & update USB Gadget for Android 15
    - Move to Android 15 AIDL impl & update overlay paths
    - Add init-time cheat for early config before HAL takes over
    - Manually add package instead of using crDroid build flags -- I set props in vendor instead of product for sdm845

--- common.mk
+++ common.mk
@@ -195,6 +195,10 @@ PRODUCT_PACKAGES += \
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# DeviceAsWebcam
+PRODUCT_PACKAGES += \
+    DeviceAsWebcam
+
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
@@ -391,7 +395,7 @@ PRODUCT_PACKAGES_DEBUG += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti \
+    android.hardware.usb.gadget-service.qti \
     usb_compositions.conf
 
 # Vibrator
