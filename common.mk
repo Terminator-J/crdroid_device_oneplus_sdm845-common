4c6e00e9a554c4202567db3e27a65f1cfd417de7
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-04-13 17:15:03 -0700

    [TEST] sdm845-common: thermal: Try the old magicks

--- common.mk
+++ common.mk
@@ -390,6 +390,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal
 
 # Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
